6507cbc37b9bddb55c24207454b877a129f175c9
ranellran <115517307+ranellran@users.noreply.github.com>
2024-01-10 21:35:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -21,6 +21,28 @@ font = pygame.font.SysFont('forte', 20)
 canvas = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Mario')
 
+def game_over():
+    pygame.mixer.music.stop()
+    music = pygame.mixer.Sound('mario_dies.wav')
+    music.play()
+    topscore.top_score(SCORE)
+    game_over_img = pygame.image.load('end.png')
+    game_over_img_rect = game_over_img.get_rect()
+    game_over_img_rect.center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
+    canvas.blit(game_over_img, game_over_img_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                music.stop()
+                game_loop()
+        pygame.display.update()
+
 
 class Topscore:
     def __init__(self):
