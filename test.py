756b44b6c3f1b2b4967ce75e4dd1d36a26cd095a
ranellran <115517307+ranellran@users.noreply.github.com>
2024-01-10 21:34:31 +0800

    Update test.py

--- test.py
+++ test.py
@@ -106,27 +106,7 @@ class Mario:
             self.mario_img_rect.bottom += 10
 
 
-def game_over():
-    pygame.mixer.music.stop()
-    music = pygame.mixer.Sound('mario_dies.wav')
-    music.play()
-    topscore.top_score(SCORE)
-    game_over_img = pygame.image.load('end.png')
-    game_over_img_rect = game_over_img.get_rect()
-    game_over_img_rect.center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
-    canvas.blit(game_over_img, game_over_img_rect)
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-                music.stop()
-                game_loop()
-        pygame.display.update()
+
 
 
 def start_game():
